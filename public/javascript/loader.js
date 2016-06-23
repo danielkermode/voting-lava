afe314dd786f9719108420dc083d1b3ba22c15ee
Dan K <danielkermode@hotmail.co.nz>
2016-06-23 23:16:42 +1200

    Dank (#11)
    * npm database script
    
    * socket server client setup
    
    * readme and dir structure
    
    * remove require data.json in routes
    
    * remove ALL semicolons
    
    * I missed some apparently

--- public/javascript/loader.js
+++ public/javascript/loader.js
@@ -1,17 +1,17 @@
 var progress = setInterval(function () {
-    var $bar = $("#bar");
+    var $bar = $("#bar")
 
     if ($bar.width() >= 600) {
-        clearInterval(progress);
+        clearInterval(progress)
     } else {
-        $bar.width($bar.width() + 60);
+        $bar.width($bar.width() + 60)
     }
-    $bar.text($bar.width() / 6 + "%");
-}, 800);
+    $bar.text($bar.width() / 6 + "%")
+}, 800)
 
 $(window).load(function() {
-  $("#bar").width(600);
-  $(".loader-container").fadeOut(1250);
+  $("#bar").width(600)
+  $(".loader-container").fadeOut(1250)
   setInterval(function() {$(".loader").css('display', 'none')}, 1000)
   setInterval(function() {$(".content").css('display', 'table')}, 1000)
-});
+})
