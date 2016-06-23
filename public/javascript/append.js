02eb57d56377feb6eb82122ca78768483000b6f6
Sentry <marcus@thingsima.de>
2016-06-23 14:32:57 +1200

    Adding choices is working

---
+++ public/javascript/append.js
@@ -0,0 +1,11 @@
+var numOfChoices = 2
+
+function addOption() {
+  numOfChoices++
+  $('.choice-holder').append('<div class="form-group"><label for="entry' + numOfChoices + '"class="col-sm-2 control-label">Option ' + numOfChoices + '</label><input type="text" class="form-control" id="entry' + numOfChoices + '"placeholder="Please enter a choice"></div>'
+}
+
+function removeOption() {
+  numOfChoices--
+  $('.choice-holder:last-child').remove()
+}
