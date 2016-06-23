b8783f2a59f1681abfc869d5ab0372b6fcf76084
danielkermode <danielkermode@hotmail.co.nz>
2016-06-23 23:05:04 +1200

    remove ALL semicolons

--- routes/questions.js
+++ routes/questions.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var dbConfig = require('../db-config')
 var knex = dbConfig.knex
 var config = dbConfig.config
@@ -8,54 +8,56 @@ var db = require('../lib/index')(knex)
 
 /* GET questions listing. */
 router.get('/', function(req, res) {
-  //res.redirect('/questions/new');
-  res.send('cannot list questions at the moment');
-});
+  //res.redirect('/questions/new')
+  res.send('cannot list questions at the moment')
+})
 
 router.get('/new', function(req, res) {
-  res.render('index');
-});
+  res.render('index')
+})
 
 router.post('/', function(req, res){
   db.createNewQuestion(req.body)
     .then(function(id){
-      res.redirect('/questions/' + id[0] + '/edit');
+      res.redirect('/questions/' + id[0] + '/edit')
     })
     .catch(function(error){
       console.log(error)
-      res.render('error',error);
+      res.render('error',error)
     })
-});
+})
 
 router.get('/:id/edit', function(req, res) {
   db.buildPageObject(req.params.id)
     .then(function(pageObject) {
-      res.render('pollOptions', pageObject);
+      res.render('pollOptions', pageObject)
     })
     .catch(function(error) {
       console.log(error)
-      res.render('error', error);
+      res.render('error', error)
     })
-});
+})
 
 router.post('/:id', function(req, res){
   db.updateQuestion(req.params.id,req.body)
     .then(function(){
-      res.redirect('/questions/' + req.params.id);
+      res.redirect('/questions/' + req.params.id)
     })
     .catch(function(error){
-      res.render('error', error);
+      console.log(error)
+      res.render('error', error)
     })
-});
+})
 
 router.get('/:id', function(req, res) {
   db.buildPageObject(req.params.id)
     .then(function(pageObject) {
-      res.render('pollView', pageObject);
+      res.render('pollView', pageObject)
     })
     .catch(function(error) {
-      res.render('error', error);
+      console.log(error)
+      res.render('error', error)
     })
-});
+})
 
-module.exports = router;
+module.exports = router
