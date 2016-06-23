749435456c8240fc87e6dc7f796d35adb7f441cc
Sentry <marcus@thingsima.de>
2016-06-23 15:30:36 +1200

    Modified some fade variables and added global CSS

--- public/javascript/loader.js
+++ public/javascript/loader.js
@@ -7,14 +7,11 @@ var progress = setInterval(function () {
         $bar.width($bar.width() + 60);
     }
     $bar.text($bar.width() / 6 + "%");
-    if ($bar.width() / 6 == 100){
-      $bar.text("Still working ... " + $bar.width() / 6 + "%");
-    }
 }, 800);
 
 $(window).load(function() {
   $("#bar").width(600);
-  $(".loader-container").fadeOut(2000);
-  setInterval(function() {$(".loader").css('display', 'none')}, 2000)
-  setInterval(function() {$(".content").css('display', 'table')}, 2000)
+  $(".loader-container").fadeOut(1250);
+  setInterval(function() {$(".loader").css('display', 'none')}, 1000)
+  setInterval(function() {$(".content").css('display', 'table')}, 1000)
 });
