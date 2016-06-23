ed53ef2fec31d5fbe18a40da67f70dda185bc18f
danielkermode <danielkermode@hotmail.co.nz>
2016-06-23 22:50:57 +1200

    remove require data.json in routes

--- routes/questions.js
+++ routes/questions.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../data/data.json');
 var dbConfig = require('../db-config')
 var knex = dbConfig.knex
 var config = dbConfig.config
