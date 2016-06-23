77348dc1b5b2d1a2e5b860587e959726bbac3e62
Sentry <marcus@thingsima.de>
2016-06-23 14:53:11 +1200

    Add option is working

--- public/javascript/append.js
+++ public/javascript/append.js
@@ -2,7 +2,7 @@ var numOfChoices = 2
 
 function addOption() {
   numOfChoices++
-  $('.choice-holder').append('<div class="form-group"><label for="entry' + numOfChoices + '"class="col-sm-2 control-label">Option ' + numOfChoices + '</label><input type="text" class="form-control" id="entry' + numOfChoices + '"placeholder="Please enter a choice"></div>'
+  $('.choice-holder').append('<div class="form-group"><label for="entry' + numOfChoices + '"class="col-sm-2 control-label">Option ' + numOfChoices + '</label><input type="text" class="form-control" id="entry' + numOfChoices + '"placeholder="Please enter a choice"></div>')
 }
 
 function removeOption() {
