c0641ff9471bf034a028211d2f177ed0886cc491
danielkermode <danielkermode@hotmail.co.nz>
2016-06-25 00:19:26 +1200

    up to date

--- public/javascript/socketclient.js
+++ public/javascript/socketclient.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-var socket = io()
-=======
 var socket = io()
 socket.on('newvotes', function(data) {
   //update vote count.. TODO!!!
@@ -15,5 +12,3 @@ function updateVote(id, amount){
   })
 }
 
-updateVote(1, 1)
->>>>>>> 6e3de0ffb8f97b80089abc81151379c9e581123f
