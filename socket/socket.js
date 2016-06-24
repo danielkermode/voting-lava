ccfe613fbef186c2951263ea76f8fa61a499a15f
Dan K & George Francis <pair+danielkermode.georgeF105@devbootcamp.com>
2016-06-24 12:13:34 +1200

    add increment and decrment vote tests and functions

--- socket/socket.js
+++ socket/socket.js
@@ -2,4 +2,8 @@ module.exports = function(io) {
   io.on('connection', function(socket) {
     console.log('this is the socket id', socket.id)
   })
+
+  io.on('pollvote', function(socket) {
+    io.emit('newvotes')
+  })
 }
